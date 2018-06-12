182fa71fca52f586409ddae7b8dad4442ed281ba
zhujinqiang1989 <523777698@qq.com>
2018-06-12 19:27:32 +0800

    add a println line

--- src/com/zhujq/learn/HelloWorld.java
+++ src/com/zhujq/learn/HelloWorld.java
@@ -3,5 +3,7 @@ package com.zhujq.learn;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+
     }
 }
